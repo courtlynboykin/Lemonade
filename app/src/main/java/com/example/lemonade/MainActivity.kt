d537e3e420755a343c67825eebcfb0a19b684871
courtlynboykin <courtlynb@google.com>
2023-11-12 18:18:54 -0500

    Added a "when" conditional to show all screens when they are clicked, replaced button with "clickable" modifier for each column

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -5,13 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -23,10 +21,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lemonade.ui.theme.LemonadeTheme
 
@@ -35,7 +32,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LemonadeTheme {
-                    LemonadeApp()
+                LemonadeApp()
             }
         }
     }
@@ -54,76 +51,100 @@ fun LemonadeApp() {
 
 @Composable
 fun MakeLemonade(modifier: Modifier = Modifier) {
-    var result by remember { mutableStateOf (1) }
-    val image = when (result) {
-        1 -> R.drawable.lemon_tree
-        2 -> R.drawable.lemon_squeeze
-        3 -> R.drawable.lemon_drink
-        4 -> R.drawable.lemon_restart
-        else -> R.drawable.lemon_tree
-    }
-    val instructions = when (result) {
-        1 -> R.string.tree
-        2 -> R.string.tap
-        3 -> R.string.drink
-        4 -> R.string.restart
-        else -> R.string.tree
-    }
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
-            .fillMaxSize()
-            .background(Color.LightGray)
-    ) {
-        Box {
-            Button(
-                onClick = { result++ },
-                shape = RectangleShape,
-                colors = ButtonDefaults.buttonColors(Color.Yellow)
-            ) {
-                Image(
-                    painter = painterResource(image),
-                    contentDescription = stringResource(R.string.tree),
-                    modifier = modifier
-                        .background(Color.Yellow)
-                )
-            }
-        }
-            Text(
-                text = stringResource(instructions)
-            )
-        }
-
-    }
-
-
-
-
-@Composable
-fun LemonSqueeze(modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
-            .fillMaxSize()
-            .background(Color.LightGray)
-    ) {
-        Box {
-            Image(
-                painter = painterResource(R.drawable.lemon_squeeze),
-                contentDescription = stringResource(R.string.lemon),
-                modifier = modifier
-                    .background(Color.Yellow)
-            )
-        }
+    Column {
         Text(
-            text = stringResource(R.string.tap)
+            text = "Lemonade",
+            textAlign = TextAlign.Center,
+            modifier = modifier
+                .background(Color.Yellow)
+                .fillMaxWidth()
         )
+        var result by remember { mutableStateOf(1) }
+        when (result) {
+            1 ->
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = modifier
+                        .fillMaxSize()
+                        .background(Color(0xFFd2e8d4))
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.lemon_tree),
+                        contentDescription = stringResource(R.string.tree),
+                        modifier = modifier
+                            .background(Color.White)
+                            .clickable { result++ }
+                    )
+                    Text(
+                        text = stringResource(R.string.tap)
+                    )
+                }
+
+            2 ->
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = modifier
+                        .fillMaxSize()
+                        .background(Color(0xFFd2e8d4))
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.lemon_squeeze),
+                        contentDescription = stringResource(R.string.lemon),
+                        modifier = modifier
+                            .background(Color.White)
+                            .clickable { result++ }
+                    )
+                    Text(
+                        text = stringResource(R.string.squeeze)
+                    )
+                }
+
+            3 ->
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = modifier
+                        .fillMaxSize()
+                        .background(Color(0xFFd2e8d4))
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.lemon_drink),
+                        contentDescription = stringResource(R.string.glass),
+                        modifier = modifier
+                            .background(Color.White)
+                            .clickable { result++ }
+                    )
+                    Text(
+                        text = stringResource(R.string.drink)
+                    )
+                }
+
+            4 ->
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = modifier
+                        .fillMaxSize()
+                        .background(Color(0xFFd2e8d4))
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.lemon_restart),
+                        contentDescription = stringResource(R.string.empty),
+                        modifier = modifier
+                            .background(Color.White)
+                            .clickable { result = 1 }
+                    )
+                    Text(
+                        text = stringResource(R.string.restart)
+                    )
+                }
+
+        }
+
     }
-
 }
-
 
 @Preview()
 @Composable
