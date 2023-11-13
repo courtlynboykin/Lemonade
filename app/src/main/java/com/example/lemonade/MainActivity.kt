1cac402773fe25f94426c66e4db54eedcb7ec943
courtlynboykin <courtlynb@google.com>
2023-11-13 10:38:18 -0500

    added a variable to repeat the lemon squeeze multiple times, reformatted code, optimized imports, and added padding and bold text to header

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -19,7 +19,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -29,6 +29,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -57,13 +58,77 @@ fun LemonadeApp() {
 }
 
 @Composable
+fun MakeLemonade(modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = "Lemonade",
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = modifier
+                .background(Color.Yellow)
+                .fillMaxWidth()
+                .padding(20.dp)
+        )
+        var repeatSqueeze = (1..4).random()
+        var result by remember { mutableIntStateOf(1) }
+        when (result) {
+            1 ->
+                LemonTextAndImage(
+                    image = painterResource(R.drawable.lemon_tree),
+                    imageDescription = stringResource(R.string.tree),
+                    onClick = { result++ },
+                    instruction = stringResource(R.string.tap),
+                    modifier = modifier
+                )
+
+            2 ->
+                repeat(repeatSqueeze) {
+                    LemonTextAndImage(
+                        image = painterResource(R.drawable.lemon_squeeze),
+                        imageDescription = stringResource(R.string.lemon),
+                        onClick = {
+                            repeatSqueeze--
+                            if (repeatSqueeze == 0) {
+                                result++
+                            }
+                        },
+                        instruction = stringResource(R.string.squeeze),
+                        modifier = modifier
+                    )
+                }
+
+            3 ->
+                LemonTextAndImage(
+                    image = painterResource(R.drawable.lemon_drink),
+                    imageDescription = stringResource(R.string.glass),
+                    onClick = { result++ },
+                    instruction = stringResource(R.string.drink),
+                    modifier = modifier
+                )
+
+            4 ->
+                LemonTextAndImage(
+                    image = painterResource(R.drawable.lemon_restart),
+                    imageDescription = stringResource(R.string.empty),
+                    onClick = { result = 1 },
+                    instruction = stringResource(R.string.restart),
+                    modifier = modifier
+                )
+        }
+
+    }
+
+}
+
+@Composable
 fun LemonTextAndImage(
     image: Painter,
     imageDescription: String,
     onClick: () -> Unit,
     instruction: String,
     modifier: Modifier
-){
+) {
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -95,57 +160,7 @@ fun LemonTextAndImage(
     }
 }
 
-@Composable
-fun MakeLemonade(modifier: Modifier = Modifier) {
-    Column {
-        Text(
-            text = "Lemonade",
-            textAlign = TextAlign.Center,
-            modifier = modifier
-                .background(Color.Yellow)
-                .fillMaxWidth()
-        )
-        val repeatSqueeze = (1..4).random()
-        var result by remember { mutableStateOf(1) }
-        when (result) {
-            1 ->
-                LemonTextAndImage(
-                    image = painterResource(R.drawable.lemon_tree),
-                    imageDescription = stringResource(R.string.tree),
-                    onClick = { result++ },
-                    instruction = stringResource(R.string.tap),
-                    modifier = modifier
-                )
-            2 ->
-                repeat(repeatSqueeze) { LemonTextAndImage(
-                    image = painterResource(R.drawable.lemon_squeeze),
-                    imageDescription = stringResource(R.string.lemon),
-                    onClick = { result ++ },
-                    instruction = stringResource(R.string.squeeze),
-                    modifier = modifier
-                )}
-           3 ->
-               LemonTextAndImage(
-                   image = painterResource(R.drawable.lemon_drink),
-                   imageDescription = stringResource(R.string.glass),
-                   onClick = { result++ },
-                   instruction = stringResource(R.string.drink),
-                   modifier = modifier
-               )
-            4 ->
-                LemonTextAndImage(
-                    image = painterResource(R.drawable.lemon_restart),
-                    imageDescription = stringResource(R.string.empty),
-                    onClick = { result = 1 },
-                    instruction = stringResource(R.string.restart),
-                    modifier = modifier
-                )
-        }
-
-    }
-}
-
-@Preview()
+@Preview(showSystemUi = true)
 @Composable
 fun LemonadePreview() {
     LemonadeTheme {
